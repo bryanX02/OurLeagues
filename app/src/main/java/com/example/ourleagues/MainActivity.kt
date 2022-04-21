0b1605579614d1a6bfddb1016316724f05f193c8
02bryanX <bryan.xavier.2002@gmail.com>
2022-04-21 12:25:52 +0200

    Practica

--- app/src/main/java/com/example/ourleagues/MainActivity.kt
+++ app/src/main/java/com/example/ourleagues/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.ourleagues
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.ourleagues.modelo.Programador
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bryan = Programador()
+        Log.d(":::Log", bryan.getProgrammerData().toString())
+
     }
 }
