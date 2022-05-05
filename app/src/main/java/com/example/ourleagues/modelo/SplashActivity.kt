26b26d85ddfc095c9d170b98b40537da8d7145b5
02bryanX <bryan.xavier.2002@gmail.com>
2022-05-05 17:07:30 +0200

    icono y splash
    Se ha agregado el icono de la app y se activo un splash screen

---
+++ app/src/main/java/com/example/ourleagues/modelo/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.ourleagues.modelo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ourleagues.controlador.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
